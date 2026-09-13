42a32d2f155a06918bad04f284f4e23f4fc4fc4d
Crazysodaman <77500555+Crazysodaman@users.noreply.github.com>
2026-09-13 15:35:59 -0500

    add

---
+++ src/sofia/config/__init__.py
@@ -0,0 +1,11 @@
+﻿from sofia.config.defaults import create_default_configuration
+from sofia.config.model import (
+    ProviderConfiguration,
+    SofiaConfiguration,
+)
+
+__all__ = [
+    "ProviderConfiguration",
+    "SofiaConfiguration",
+    "create_default_configuration",
+]
